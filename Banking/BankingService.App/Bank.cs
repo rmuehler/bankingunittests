74189ce7791e0073988b9c1f301ef235619a470e
Robert Muehler <robertmuehler@gmail.com>
2021-05-12 14:53:32 -0400

    finished test for checking if Bank creates empty account list

---
+++ Banking/BankingService.App/Bank.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace BankingService
+{
+    public class Bank
+    {
+        public List<Account> Accounts { get; set; }
+
+        public Bank()
+        {
+            Accounts = new List<Account>() { };
+        }
+    }
+}
---
+++ Banking/BankingService.Tests/Bank_Constructor.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace BankingService.Tests
+{
+    public class Bank_Constructor
+    {
+        [Fact]
+        public void Should_CreatBankWithEmptyAccountsList()
+        {
+            // Arrange
+            Bank bank = new Bank();
+            // Assert
+            // Act
+            bank.Accounts.Should().BeEmpty();
+        }
+    }
+}
