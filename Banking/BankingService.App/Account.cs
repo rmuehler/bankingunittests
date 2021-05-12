8201df0c7c95774c48a11d08f7edfa51273f68b4
Robert Muehler <robertmuehler@gmail.com>
2021-05-12 14:03:38 -0400

    fixed gitignore

---
+++ Banking/BankingService.App/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankingService
+{
+    public class Account
+    {
+        private decimal _balance;
+        private List<string> _transactions;
+        public decimal Balance{
+            get { return _balance; } 
+        }
+        public List<string> Transactions { 
+            get { return _transactions; } 
+        }
+        public string Id { get; set; }
+        public User Owner { get; set; }
+        public Account(string id, string first, string last){
+            _balance = 0;
+            Id = id;
+            Owner = new User{ First = first, Last = last };
+            _transactions = new List<string>();
+        }
+        public void Deposit(decimal amount){
+            if (amount < 0){
+                throw new ApplicationException("Cannot deposit negative amounts");
+            }
+            _balance += amount;
+            _transactions.Add($"Deposit: ${amount}");
+        }
+
+        public void Withdrawal(decimal amount)
+        {
+            _balance -= amount;
+        }
+    }
+}
---
+++ Banking/BankingService.App/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingService
+{
+    public struct User
+    {
+        public string First;
+        public string Last;
+
+        public override string ToString() => $"{First} {Last}";
+    }
+}
