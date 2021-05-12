6e5a14eff48a80fe858157568763f750cbec8cb3
Robert Muehler <robertmuehler@gmail.com>
2021-05-12 13:54:02 -0400

    initial setup, added test for Withdrawal

---
+++ Banking/BankingService.Tests/Account_Constructor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using BankingService;
+using FluentAssertions;
+
+namespace BankingService.Tests
+{
+    public class Account_Constructor {
+        [Fact]
+        public void Should_InitializeBalanceAs0()
+        {
+            // Arrange
+            // Act
+            Account account = new Account("CX-3", "Monica", "Barns");
+            // Assert
+            account.Balance.Should().Be(0);
+        }
+        [Fact]
+        public void Should_InitializeID_WithFirstParameter()
+        {
+            // Arrange
+            // Act
+            Account account = new Account("CX-3", "Monica", "Barns");
+            // Assert
+            account.Id.Should().Be("CX-3");
+        }
+        [Fact]
+        public void Should_InitializeOwner_FirstAndLastNames_WithSecondAndThirdParameter()
+        {
+            // Arrange
+            // Act
+            Account account = new Account("CX-3", "Monica", "Barns");
+            // Assert
+            account.Owner.ToString().Should().Be("Monica Barns");
+        }
+    }
+}
